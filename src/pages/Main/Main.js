4f0acdb20364ccc4e16a98c2530125a443a777fe
theif45 <theif45@naver.com>
2023-04-25 17:54:13 +0900

    2023.04.25 search implementation ~ing

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,6 +5,7 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import BookCard from "../../components/UI/BookCard/BookCard";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { BsMenuDown } from "react-icons/bs";
 
 const mainContainer = css`
     padding: 10px;
@@ -12,8 +13,55 @@ const mainContainer = css`
 const header = css`
     display: flex;
     justify-content: space-between;
+    padding: 40px;
     height: 100px;
 `;
+
+const title = css`
+    font-size: 35px;
+    font-weight: 600;
+`;
+
+const searchItems = css`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+`;
+
+const categoryButton = css`
+    position: relative;
+    border: 1px solid #dbdbdb;
+    border-radius: 5px;
+    width: 30px;
+    height: 30px;
+    background-color: white;
+    cursor: pointer;
+`;
+
+const categoryGroup = (isOpen) => css`
+    position: absolute;
+    top: 30px;
+    right: -151px;
+    display: ${isOpen ? "flex" : "none"};
+    flex-direction: column;
+    align-items: flex-start;
+    border: 1px solid #dbdbdb;
+    border-radius: 4px;
+    padding: 5px;
+    width: 180px;
+    max-height: 100px;
+    background-color: white;
+    overflow-y: auto;
+`;
+
+const searchInput = css`
+    border: 1px solid #dbdbdb;
+    border-radius: 7px;
+    padding: 5px;
+    width: 150px;
+    height: 30px;
+`;
+
 const main = css`
     display: flex;
     flex-wrap: wrap;
@@ -24,18 +72,21 @@ const main = css`
 const Main = () => {
     const [searchParam, setSearchParam] = useState({
         page: 1,
-        categoryId: 0,
+        categoryIds: [],
         searchValue: "",
     });
     const [refresh, setRefresh] = useState(false);
+    const [categoryRefresh, setCategoryRefresh] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     const [books, setBooks] = useState([]);
+    const [lastPage, setLastPage] = useState(1);
     const lastBookRef = useRef();
+    const categoryButtonRef = useRef();
 
     useEffect(() => {
         const observerService = (entries, observer) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
-                    console.log("last book intersecting");
                     setRefresh(true);
                 }
             });
@@ -62,20 +113,80 @@ const Main = () => {
                 if (refresh) {
                     setRefresh(false);
                 }
-                setBooks([...books, ...response.data]);
+                console.log(response);
+                const totalCount = response.data.totalCount;
+                setLastPage(totalCount % 20 === 0 ? totalCount / 20 : Math.ceil(totalCount / 20));
+                setBooks([...books, ...response.data.bookList]);
                 setSearchParam({ ...searchParam, page: searchParam.page + 1 });
             },
-            enabled: refresh,
+            enabled: refresh && searchParam.page < lastPage + 1,
         }
     );
+
+    const categories = useQuery(
+        ["categories"],
+        async () => {
+            const option = {
+                headers: {
+                    Authorization: localStorage.getItem("accessToken"),
+                },
+            };
+            const response = await axios.get("http://localhost:8080/categories", option);
+            return response;
+        },
+        {
+            onSuccess: () => {
+                if (categoryRefresh) {
+                    setCategoryRefresh(false);
+                }
+            },
+            enabled: categoryRefresh,
+        }
+    );
+
+    const categoryClickHandle = (e) => {
+        e.stopPropagation();
+
+        if (isOpen && e.target === categoryButtonRef.current) {
+            setIsOpen(false);
+        } else {
+            setIsOpen(true);
+        }
+    };
+
+    const categoryCheckHandle = (e) => {
+        if (e.target.checked) {
+            setSearchParam({ ...searchParam, categoryIds: [...searchParam.categoryIds, e.target.value] });
+        } else {
+            setSearchParam({ ...searchParam, categoryIds: [...searchParam.categoryIds.filter((id) => id !== e.target.value)] });
+        }
+    };
 
     return (
         <div css={mainContainer}>
             <Sidebar />
             <header css={header}>
-                <div>도서검색</div>
-                <div>
-                    <input type="search" />
+                <div css={title}>도서검색</div>
+                <div css={searchItems}>
+                    <button css={categoryButton} onClick={categoryClickHandle} ref={categoryButtonRef}>
+                        <BsMenuDown />
+                        <div css={categoryGroup(isOpen)}>
+                            {categories.data !== undefined
+                                ? categories.data.data.map((category) => (
+                                      <div key={category.categoryId}>
+                                          <input
+                                              type="checkbox"
+                                              onChange={categoryCheckHandle}
+                                              id={"ct-" + category.categoryId}
+                                              value={category.categoryId}
+                                          />
+                                          <label htmlFor={"ct-" + category.categoryId}>{category.categoryName}</label>
+                                      </div>
+                                  ))
+                                : ""}
+                        </div>
+                    </button>
+                    <input css={searchInput} type="search" />
                 </div>
             </header>
             <main css={main}>
